2aef1a40e492023ff3870cb58f02a3f6e79b36ed
Roman Kassebaum <roman.kassebaum@kdv-dt.de>
2015-09-04 01:38:32 +0200

    Added all hpp files.

---
+++ Source/hpp/Win32/Release/MPCommonLibD.hpp
@@ -0,0 +1,79 @@
+﻿// CodeGear C++Builder
+// Copyright (c) 1995, 2015 by Embarcadero Technologies, Inc.
+// All rights reserved
+
+// (DO NOT EDIT: machine generated header) 'MPCommonLibD.dpk' rev: 30.00 (Windows)
+
+#ifndef MpcommonlibdHPP
+#define MpcommonlibdHPP
+
+#pragma delphiheader begin
+#pragma option push
+#pragma option -w-      // All warnings off
+#pragma option -Vx      // Zero-length empty class member 
+#pragma pack(push,8)
+#include <System.hpp>	// (rtl)
+#include <SysInit.hpp>
+#include <MPShellTypes.hpp>
+#include <MPCommonUtilities.hpp>
+#include <MPResources.hpp>
+#include <MPCommonObjects.hpp>
+#include <MPDataObject.hpp>
+#include <MPThreadManager.hpp>
+#include <MPShellUtilities.hpp>
+#include <Winapi.Windows.hpp>	// (rtl)
+#include <System.Character.hpp>	// (rtl)
+#include <System.Internal.ExcUtils.hpp>	// (rtl)
+#include <System.SysUtils.hpp>	// (rtl)
+#include <System.VarUtils.hpp>	// (rtl)
+#include <System.Variants.hpp>	// (rtl)
+#include <System.AnsiStrings.hpp>	// (rtl)
+#include <System.Math.hpp>	// (rtl)
+#include <System.Generics.Defaults.hpp>	// (rtl)
+#include <System.Rtti.hpp>	// (rtl)
+#include <System.TypInfo.hpp>	// (rtl)
+#include <System.Classes.hpp>	// (rtl)
+#include <System.TimeSpan.hpp>	// (rtl)
+#include <System.DateUtils.hpp>	// (rtl)
+#include <System.IOUtils.hpp>	// (rtl)
+#include <System.Win.Registry.hpp>	// (rtl)
+#include <Vcl.Graphics.hpp>	// (vcl)
+#include <System.Actions.hpp>	// (rtl)
+#include <Vcl.ActnList.hpp>	// (vcl)
+#include <System.HelpIntfs.hpp>	// (rtl)
+#include <System.SyncObjs.hpp>	// (rtl)
+#include <Winapi.UxTheme.hpp>	// (rtl)
+#include <Vcl.GraphUtil.hpp>	// (vcl)
+#include <Vcl.StdCtrls.hpp>	// (vcl)
+#include <Winapi.ShellAPI.hpp>	// (rtl)
+#include <Vcl.Printers.hpp>	// (vcl)
+#include <Vcl.Clipbrd.hpp>	// (vcl)
+#include <Vcl.ComCtrls.hpp>	// (vcl)
+#include <Vcl.Dialogs.hpp>	// (vcl)
+#include <Vcl.ExtCtrls.hpp>	// (vcl)
+#include <Vcl.Themes.hpp>	// (vcl)
+#include <System.Win.ComObj.hpp>	// (rtl)
+#include <Winapi.FlatSB.hpp>	// (rtl)
+#include <Vcl.Forms.hpp>	// (vcl)
+#include <Vcl.Menus.hpp>	// (vcl)
+#include <Winapi.MsCTF.hpp>	// (rtl)
+#include <Vcl.Controls.hpp>	// (vcl)
+#include <Vcl.AxCtrls.hpp>	// (vcl)
+
+//-- user supplied -----------------------------------------------------------
+
+namespace Mpcommonlibd
+{
+//-- forward type declarations -----------------------------------------------
+//-- type declarations -------------------------------------------------------
+//-- var, const, procedure ---------------------------------------------------
+}	/* namespace Mpcommonlibd */
+#if !defined(DELPHIHEADER_NO_IMPLICIT_NAMESPACE_USE) && !defined(NO_USING_NAMESPACE_MPCOMMONLIBD)
+using namespace Mpcommonlibd;
+#endif
+#pragma pack(pop)
+#pragma option pop
+
+#pragma delphiheader end.
+//-- end unit ----------------------------------------------------------------
+#endif	// MpcommonlibdHPP
